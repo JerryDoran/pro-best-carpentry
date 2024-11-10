0654fc4149d730abaa5ec592dab3cc44c6887627
Jerome Doran <disneyfan9184@gmail.com>
2024-11-10 08:54:20 -0500

    fixed layout

--- app/page.tsx
+++ app/page.tsx
@@ -6,10 +6,7 @@ import Image from 'next/image';
 export default function HomePage() {
   return (
     <main className=''>
-      <div className='relative bg-home-img bg-cover bg-no-repeat bg-fixed '>
-        <div className='absolute inset-0 bg-black opacity-40 h-[65vh]' />
-        <Hero />
-      </div>
+      <Hero />
       <WhyHireMe />
     </main>
   );
--- components/hero.tsx
+++ components/hero.tsx
@@ -2,8 +2,9 @@ import Link from 'next/link';
 
 export default function Hero() {
   return (
-    <div>
-      <section className='h-[65vh] flex flex-col items-center justify-center gap-6 relative text-white z-10 p-4'>
+    <div className='relative bg-home-img bg-center bg-cover bg-no-repeat bg-fixed'>
+      <div className='absolute inset-0 bg-black opacity-40 h-[100%] w-[100%]' />
+      <section className='h-[100%] w-[100%] flex flex-col items-center justify-center gap-6 relative text-white z-10 p-4'>
         <h1 className='opacity-0 animate-fadeIn text-4xl md:text-7xl font-bold mt-8 text-center'>
           Pro Best Carpentry
         </h1>
@@ -19,7 +20,7 @@ export default function Hero() {
           Call Now <span className='text-base font-medium'> 330-550-0088</span>
         </div>
       </section>
-      <div className='grid md:grid-cols-2 lg:grid-cols-4 w-full text-white animate-fadeIn delay-700 opacity-0'>
+      <div className='grid md:grid-cols-2 lg:grid-cols-4 w-full text-white animate-fadeIn delay-700 opacity-0 mt-44'>
         <Link
           href='/'
           className='flex-1 bg-neutral-300 h-[200px] flex justify-center items-center p-6 group relative overflow-hidden'
